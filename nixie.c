a2e934298baa42fc9b9dc38076d77444739f18d3
Luke Maurits <luke@maurits.id.au>
2017-10-31 19:06:41 +0200

    Poll and debounce switches in the main loop, and use them to set the time.

--- nixie.c
+++ nixie.c
@@ -116,12 +116,87 @@ void update_hours(uint8_t *tubes) {
 	tubes[0] = (uint8_t) ((time >> 4) & 0x03);
 }
 
+void reset_seconds() {
+	i2c_start_wait(RTC+I2C_WRITE);
+	i2c_write(0x00);
+	i2c_write(0x80);
+	i2c_stop();
+	tubes[4] = 0;
+	tubes[5] = 0;
+}
+
+void increment_minutes() {
+	uint8_t mins, tens, units;
+	mins = read_rtc(0x01);
+	units = mins & 0b00001111;
+	tens = mins >> 4;
+	units++;
+	if(units == 10) {
+		units = 0;
+		tens++;
+	}
+	if(tens == 6) tens = 0;
+	mins = units | (tens << 4);
+	i2c_start_wait(RTC+I2C_WRITE);
+	i2c_write(0x01);
+	i2c_write(mins);
+	i2c_stop();
+	update_minutes(tubes);
+}
+
+void increment_hours() {
+	uint8_t hours, tens, units;
+	hours = read_rtc(0x02);
+	units = hours & 0b00001111;
+	tens = (hours >> 4) & 0b00000011;
+	units++;
+	if(units == 10) {
+		units = 0;
+		tens++;
+	}
+	if(tens == 2 && units == 5) {
+		units = 0;
+		tens = 0;
+	}
+	hours = units | (tens << 4);
+	i2c_start_wait(RTC+I2C_WRITE);
+	i2c_write(0x02);
+	i2c_write(hours);
+	i2c_stop();
+	update_hours(tubes);
+}
+
+uint8_t handle_buttons(uint8_t old_buttons) {
+	uint8_t new_buttons, raw_buttons, count;
+	count = 0;
+	new_buttons = PINC | 0b11111000;
+	_delay_ms(10);
+	while(count < 5) {
+		raw_buttons = PINC | 0b11111000;
+		if(raw_buttons == new_buttons) {
+			count++;
+		} else {
+			count = 0;
+			new_buttons = raw_buttons;
+		}
+		_delay_ms(10);
+	}
+	if(new_buttons != old_buttons) {
+		if(!(new_buttons & 0b00000001)) reset_seconds();
+		if(!(new_buttons & 0b00000010)) increment_minutes();
+		if(!(new_buttons & 0b00000100)) increment_hours();
+	}
+	return new_buttons;
+}
+
 int main() {
+
+	uint8_t buttons = 0b00000111;
 
 	// Set port directions
 	DDRB = 0b00111111;
 	DDRD = 0b11111110;
-
+	PORTC = 0b00000111;
 	// Start RTC oscillator
 	i2c_init();
 	init_rtc();
@@ -135,7 +210,7 @@ int main() {
 		update_seconds(tubes);
 		update_minutes(tubes);
 		update_hours(tubes);
-		_delay_ms(100);
+		buttons = handle_buttons(buttons);
 	}
 
 	return 0;
